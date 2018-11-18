3292bb29d0f959af9bc85329302248fdecf7340f
AKubax <ovmiovmiovmi@gmail.com>
2018-11-18 18:56:07 +0300

    say no to govnocode

--- spyMorse.cpp
+++ spyMorse.cpp
@@ -5,54 +5,121 @@
 #include <conio.h>
 #include <windows.h>
 
+const int DotDurationMSec = 250;
+const int DotDuration     = 1;
 
-AKQueue<char> TheQueue(80);
+const int DashDuration            = 3 * DotDuration;
+const int SpaceInLetterDuration   = 1 * DotDuration;
+const int SpaceInWordDuration     = 3 * DotDuration;
+const int SpaceInSentenceDuration = 7 * DotDuration;
+
+const int DumpYShift = 500;
+const int DumpXShift = 75;
+
+const int BeepFreq = 575;
+
+
+AKQueue<char> TheQueue(30);
 
 const char* morseAlphabet[26] = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
 const char* morseDigits[10]   = { "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----." };
 
+
+void dump(AKQueue<char>& queue, int x, int y){
+    POINT prevPos = txGetConsoleCursorPos();
+
+    txSetConsoleCursorPos(x, y);
+    for(const char* ptr = queue.getBuf(); ptr < queue.getBuf() + queue.getSz(); ptr++){
+        if(queue.empty()) { $r; }
+
+        else if(queue.getBegin() < queue.getEnd()? (ptr >= queue.getBegin() && ptr < queue.getEnd()) : ( ptr >= queue.getBegin() || ptr < queue.getEnd() ) ) { $b; }
+        else                                                                                            { $r; }
+
+        printf("%c", (isspace(*ptr) || !(*ptr))? '_' : *ptr);
+
+    }
+
+    $b;
+
+    POINT fontSize = txGetConsoleFontSize();
+
+    txSetConsoleCursorPos(x, y + fontSize.y);
+    printf("%*s", queue.getSz() + 20, "");
+    txSetConsoleCursorPos(x, y + 2 * fontSize.y);
+    printf("%*s", queue.getSz() + 20, "");
+
+    txSetConsoleCursorPos(x, y - fontSize.y);
+    printf("%*s", queue.getSz() + 20, "");
+    txSetConsoleCursorPos(x, y - 2 * fontSize.y);
+    printf("%*s", queue.getSz() + 20, "");
+
+    txSetConsoleCursorPos(x + (queue.getEnd() - queue.getBuf()) * fontSize.x, y + 1 * fontSize.y);
+    printf("^");
+    txSetConsoleCursorPos(x + (queue.getEnd() - queue.getBuf()) * fontSize.x, y + 2 * fontSize.y);
+    printf("T = 0x%p (%d)", queue.getEnd(), queue.getEnd() - queue.getBuf());
+
+    $y;
+    txSetConsoleCursorPos(x + (queue.getBegin() - queue.getBuf()) * fontSize.x, y - 1 * fontSize.y);
+    printf("v");
+    txSetConsoleCursorPos(x + (queue.getBegin() - queue.getBuf()) * fontSize.x, y - 2 * fontSize.y);
+    printf("H");
+    printf(" = 0x%p (%d)", queue.getBegin(), queue.getBegin() - queue.getBuf());
+
+
+    $d
+    txSetConsoleCursorPos(prevPos.x, prevPos.y);
+}
+
+
 void sleepFor(unsigned int n);
 void sleepFor(unsigned int n){
-    std::this_thread::sleep_for(std::chrono::milliseconds(250 * n));
+    std::this_thread::sleep_for(std::chrono::milliseconds(DotDurationMSec * n));
 }
 
 void printMorseLetter(const char* c);
 void printMorseLetter(const char* c){
     for(const char* ptr = c; *ptr != '\0'; ptr++){
-        printf("%c", *ptr);
 
-        if(*ptr == '.') Beep(575, 250);
-        else            Beep(575, 750);
+        printf("%c", *ptr);
+        dump(TheQueue, DumpXShift, DumpYShift);
+
+        if(*ptr == '.') Beep(BeepFreq, DotDurationMSec *  DotDuration);
+        else            Beep(BeepFreq, DotDurationMSec * DashDuration);
 
         if(*(ptr + 1) == '\0') break;
 
-        printf(" ");
-        sleepFor(1);
+        sleepFor(SpaceInLetterDuration);
     }
 }
 
 void printLetter(char c);
 void printLetter(char c){
+
+
     if(std::isspace(c)){
-        (c == ' ' || c == '\t')? printf("%7c", c) : printf("%c", c);
+        (c == ' ' || c == '\t')? printf("%2c", c) : printf("%c", c);
         if(c == '\r') printf("\n");
-        sleepFor(7);
+        sleepFor(SpaceInSentenceDuration);
     }
     else if(std::isalpha(c)){
         printMorseLetter(morseAlphabet[std::toupper(c) - 'A']);
-        sleepFor(3);
-        printf("%3c", ' ');
+        sleepFor(SpaceInWordDuration);
+        printf("%1c", ' ');
     }
     else if(std::isdigit(c)){
         printMorseLetter(morseDigits[c - '0']);
-        sleepFor(3);
-        printf("%3c", ' ');
+        sleepFor(SpaceInWordDuration);
+        printf("%1c", ' ');
     }
+
+
 }
 
 void printFunc();
 void printFunc(){
-    for(;;) if(!TheQueue.empty()) printLetter(TheQueue.pop());
+    for(;;){
+        if(!TheQueue.empty())  printLetter(TheQueue.pop());
+    }
 }
 
 
@@ -65,12 +132,16 @@ int main(){
 //    TheQueue.push('\n');
 //    TheQueue.push('d');
 
+    InitializeCriticalSection(&CriticalSection);
+
+
     std::thread t2(printFunc);
 
     while(1){
         char c = getch();
 
         TheQueue.push(c);
+
     }
 
     t2.join();
